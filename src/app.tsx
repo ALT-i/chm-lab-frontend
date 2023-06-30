b7959b65ce5b1b0275a1b77813a65b8fd910722a
JhimmieC137 <toluwalope.david@gmail.com>
2023-06-30 21:37:48 +0100

    added component and styling

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,6 @@
-// import React from "react";
-
-// export default function App() {
-//     return (
-//         <div>
-//             <h1>Hey there!</h1>
-//         </div>
-//     );
-// }
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Route } from "react-router";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// styles
-// import "./assets/scss/index.scss";
 
 //pages
 import Index from './views/index';
